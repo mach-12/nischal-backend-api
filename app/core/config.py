c2042f20c09aab43c9674d611acdd60840d165b8
mach-12 <89384052+mach-12@users.noreply.github.com>
2023-08-08 16:52:31 +0530

    changed .env method

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,9 @@
 from pydantic_settings import BaseSettings
-from decouple import config
+from dotenv import load_dotenv
+import os
 
 class Settings(BaseSettings):
+    # load_dotenv(dotenv_path='app/core/.env')
     API_V1_STR: str = "/api/v1"
 
     # BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
@@ -11,27 +13,27 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Cybershell Backend API"
 
     FIREBASE_CONFIG: dict = {
-        "apiKey": config("FIREBASE_API_KEY"),
-        "authDomain": config("FIREBASE_AUTH_DOMAIN"),
-        "projectId": config("FIREBASE_PROJECT_ID"),
-        "storageBucket": config("FIREBASE_STORAGE_BUCKET"),
-        "messagingSenderId": config("FIREBASE_MESSAGING_SENDER_ID"),
-        "appId": config("FIREBASE_APP_ID"),
-        "databaseURL": config("FIREBASE_DATABASE_URL"),
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID"),
+        "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
     }
 
     CERTIFICATE: dict = {
-        "type": config("CERTIFICATE_TYPE"),
-        "project_id": config("CERTIFICATE_PROJECT_ID"),
-        "private_key_id": config("CERTIFICATE_PRIVATE_KEY_ID"),
-        "private_key": config("CERTIFICATE_PRIVATE_KEY").replace(r'\n', '\n'),
-        "client_email": config("CERTIFICATE_CLIENT_EMAIL"),
-        "client_id": config("CERTIFICATE_CLIENT_ID"),
-        "auth_uri": config("CERTIFICATE_AUTH_URI"),
-        "token_uri": config("CERTIFICATE_TOKEN_URI"),
-        "auth_provider_x509_cert_url": config("CERTIFICATE_AUTH_PROVIDER_X509_CERT_URL"),
-        "client_x509_cert_url": config("CERTIFICATE_CLIENT_X509_CERT_URL"),
-        "universe_domain": config("CERTIFICATE_UNIVERSE_DOMAIN"),
+        "type": os.getenv("CERTIFICATE_TYPE"),
+        "project_id": os.getenv("CERTIFICATE_PROJECT_ID"),
+        "private_key_id": os.getenv("CERTIFICATE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("CERTIFICATE_PRIVATE_KEY").replace(r'\n', '\n'),
+        "client_email": os.getenv("CERTIFICATE_CLIENT_EMAIL"),
+        "client_id": os.getenv("CERTIFICATE_CLIENT_ID"),
+        "auth_uri": os.getenv("CERTIFICATE_AUTH_URI"),
+        "token_uri": os.getenv("CERTIFICATE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("CERTIFICATE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CERTIFICATE_CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("CERTIFICATE_UNIVERSE_DOMAIN"),
     }
 
     
